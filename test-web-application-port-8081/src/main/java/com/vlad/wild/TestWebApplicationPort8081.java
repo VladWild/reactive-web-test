dbfd06124daa2935d1d1b9722ce9c7d8062a4e21
VladWild <33370139+VladWild@users.noreply.github.com>
2023-11-19 20:56:40 +0300

    add test-web-application-port-8081 module

---
+++ test-web-application-port-8081/src/main/java/com/vlad/wild/TestWebApplicationPort8081.java
@@ -0,0 +1,12 @@
+package com.vlad.wild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebApplicationPort8081 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWebApplicationPort8081.class, args);
+    }
+}
---
+++ test-web-application-port-8081/src/main/java/com/vlad/wild/web/TestController.java
@@ -0,0 +1,27 @@
+package com.vlad.wild.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+public class TestController {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @GetMapping("/{delay}")
+    public ResponseEntity<Void> delay(@PathVariable("delay") Long delaySec) throws InterruptedException {
+        long start = System.currentTimeMillis();
+
+        logger.info("Start delay");
+        Thread.sleep(delaySec * 1000);
+        logger.info("End delay {} ms", System.currentTimeMillis() - start);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
